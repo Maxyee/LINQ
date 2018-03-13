a8886fb86757caf3e03053beafe24fe79abdfdc0
Maxyee <eyaminkhan00@gmail.com>
2018-03-14 04:56:03 +0600

    LinQ extention method

---
+++ LinQ-3/ExtentionMethodLinQ/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtentionMethodLinQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strName = "mohon";
+            //string result = strName.ChangeFirstLetterCase();
+
+            //string result = StringHelper.ChangeFirstLetterCase(strName);
+
+            //extention method it is.....
+            string result = strName.ChangeFirstLetterCase();
+            Console.WriteLine(result);
+
+
+            List<int> Numbers = new List<int>{1,2,3,4,5,6,7,8,9,10};
+
+            //IEnumerable<int> EvenNumbers = Numbers.Where(n => n%2 == 0);
+            IEnumerable<int> EvenNumbers = Enumerable.Where(Numbers, n => n % 2 == 0);
+
+            foreach (int evenNumber in EvenNumbers)
+            {
+                Console.WriteLine(evenNumber);
+            }
+            Console.ReadKey();
+        }
+    }
+}
