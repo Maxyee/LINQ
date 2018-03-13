fa5ed9abfbe3d8f8200f90b97548277b79b20ce9
Maxyee <eyaminkhan00@gmail.com>
2018-03-14 04:32:47 +0600

    Linq and lambda expression

---
+++ LinQ-2/Student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LinQ_2
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+
+        public static List<Student> GetAllStudents()
+        {
+            List<Student> listStudents = new List<Student>();
+
+            Student student1 = new Student
+            {
+                ID = 101,
+                Name = "Mark",
+                Gender = "Male"
+            };
+            listStudents.Add(student1);
+
+            Student student2 = new Student
+            {
+                ID = 101,
+                Name = "Mery",
+                Gender = "Female"
+            };
+            listStudents.Add(student2);
+
+            Student student3 = new Student
+            {
+                ID = 101,
+                Name = "Jon",
+                Gender = "Male"
+            };
+            listStudents.Add(student3);
+
+            Student student4 = new Student
+            {
+                ID = 101,
+                Name = "Bill",
+                Gender = "Male"
+            };
+            listStudents.Add(student4);
+
+            return listStudents;
+        }
+    }
+}
---
+++ LinQ-2/WebForm1.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LinQ_2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // LinQ Query With Lambda Expression
+            IEnumerable<Student> queryResult =  Student.GetAllStudents().Where(s => s.Gender == "Male");
+
+            GridView1.DataSource = queryResult;
+            GridView1.DataBind();
+
+
+            // LinQ to Sql Query
+            IEnumerable<Student> queryResult2 = from s in Student.GetAllStudents()
+                                                where s.Gender == "Male"
+                                                select s;
+
+            GridView2.DataSource = queryResult2;
+            GridView2.DataBind();
+        }
+    }
+}
